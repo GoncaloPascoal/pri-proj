d8b4a3f77948a949626a4db7ef7364ec12233714
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2021-11-05 01:14:32 +0000

    Change arquitecture to promise-based requests

--- proton_db.py
+++ proton_db.py
@@ -1,20 +1,20 @@
-import requests
+from requests_futures.sessions import FuturesSession
 import pandas as pd
+from tqdm import tqdm
 
-def parse_page(game_id):
-    url = f"https://www.protondb.com/api/v1/reports/summaries/{game_id}.json"
-    answer = requests.get(url)
+def parse_future(future):
+    answer = future.result()
 
     if answer.status_code == 404:
         return {
-            "appid" : game_id,
+            "appid" : future.appid,
             "protondb_reports" : 0,
             "protondb_tier" : "unknown",
         }
     else:
         data = answer.json()
         return {
-            "appid" : game_id,
+            "appid" : future.appid,
             "protondb_reports" : data["total"],
             "protondb_tier" : data["tier"],
         }
@@ -22,12 +22,19 @@ def parse_page(game_id):
 def main():
     games = pd.read_csv('data/steam.csv')
     appids = list(games["appid"])
-    
-    res = []
 
-    for appid in appids:
-        res.append(parse_page(appid))
+    session = FuturesSession(max_workers=10)
     
+    futures=[]
+    for appid in appids:
+        future = session.get(f"https://www.protondb.com/api/v1/reports/summaries/{appid}.json")
+        future.appid = appid
+        futures.append(future)
+
+    res = []
+    for future in tqdm(futures):
+        res.append(parse_future(future))
+
     pd.DataFrame(res).to_csv('data/proton_db.csv', index=False)
 
 if __name__ == "__main__":
