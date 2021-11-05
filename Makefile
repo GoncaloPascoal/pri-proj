22ba6778af5db5582909d7eee726cd51105c5f6b
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2021-11-05 01:52:36 +0000

    Add saving checkpoints

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ run :
 	python3 prepare.py
 
 install-dependencies :
-	for dep in pandas seaborn beautifulsoup4 steamreviews colorama requests; do \
+	for dep in pandas seaborn beautifulsoup4 steamreviews colorama requests requests_futures tqdm; do \
 		pip3 install $$dep ; \
 	done
--- proton_db.py
+++ proton_db.py
@@ -2,6 +2,8 @@ from requests_futures.sessions import FuturesSession
 import pandas as pd
 from tqdm import tqdm
 
+PROTON_CSV = 'data/proton_db.csv'
+
 def parse_future(future):
     answer = future.result()
 
@@ -19,6 +21,14 @@ def parse_future(future):
             "protondb_tier" : data["tier"],
         }
 
+def get_missing_appids(appids):
+    try:
+        already_done_df = pd.read_csv(PROTON_CSV)
+        already_done = set(list(already_done_df["appid"]))
+        return list(set(appids) - already_done)
+    except FileNotFoundError:
+        return appids
+
 def main():
     games = pd.read_csv('data/steam.csv')
     appids = list(games["appid"])
@@ -26,16 +36,24 @@ def main():
     session = FuturesSession(max_workers=10)
     
     futures=[]
-    for appid in appids:
+    for appid in get_missing_appids(appids):
         future = session.get(f"https://www.protondb.com/api/v1/reports/summaries/{appid}.json")
         future.appid = appid
         futures.append(future)
 
-    res = []
-    for future in tqdm(futures):
+    try:
+        already_done_df = pd.read_csv(PROTON_CSV)
+        res = already_done_df.to_dict('records') # Lets continue from where we left off
+    except FileNotFoundError:
+        res = []
+
+    for i, future in enumerate(tqdm(futures)):
         res.append(parse_future(future))
 
-    pd.DataFrame(res).to_csv('data/proton_db.csv', index=False)
+        if i % 1000 == 0:
+            pd.DataFrame(res).to_csv(PROTON_CSV, index=False)
+
+    pd.DataFrame(res).to_csv(PROTON_CSV, index=False)
 
 if __name__ == "__main__":
     main()
