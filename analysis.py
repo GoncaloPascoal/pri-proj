7e15182816d627a2dd813561738a6ba178244089
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-15 20:05:12 +0000

    Add positive reviews per playtime plot

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import seaborn as sb
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,25 +13,22 @@ PROTON_DB_TIERS = ['borked', 'bronze', 'silver', 'gold', 'platinum']
 def sort_owners(x):
     return int(x.split('-')[0])
 
+def shorten_num(x):
+    x = int(x)
+    if x >= 1000000:
+        return str(x // 1000000) + 'M'
+    elif x >= 1000:
+        return str(x // 1000) + 'K'
+    else:
+        return str(x)
+
 def rename_owners(x):
-    def f(x):
-        x = int(x)
-        if x >= 1000000:
-            return str(x // 1000000) + 'M'
-        elif x >= 1000:
-            return str(x // 1000) + 'K'
-        else:
-            return str(x)
-    
     parts = x.split('-')
     low, high = parts[0], parts[1]
-    return '-'.join([f(low), f(high)])
+    return '-'.join([shorten_num(low), shorten_num(high)])
 
 def main():
     steam_df = pd.read_csv('data/steam.csv')
-    hltb_df = pd.read_csv('data/hltb.csv')
-    proton_df = pd.read_csv('data/proton_db.csv')
-    reviews_df = pd.read_csv('data/reviews.csv')
 
     steam_df = process_steam_data(steam_df)
     steam_df['review_score'] = (steam_df['positive_ratings'] /
@@ -38,8 +36,54 @@ def main():
 
     # print(steam_df.sort_values('review_score', ascending=False)[['name', 'review_score']].head(20))
     
-    # Review score distribution
-    sb.histplot(data=steam_df, x='review_score', bins=20).set(title="Review Score Distribution", ylabel="Count", xlabel="Review Score")
+    descriptions_df = pd.read_json('data/steam.json')[['appid', 'detailed_description']]
+    descriptions_df['word_count'] = descriptions_df['detailed_description'].apply(lambda x: len(x.split()))
+
+    # Description word count
+    # sb.histplot(data=descriptions_df, x='word_count', bins=50, binrange=(0, 2000)).set(
+    #     title='Description Word Count',
+    #     ylabel='Count',
+    # )
+    # plt.show()
+
+    # # Review score distribution
+    # steam_df['total_ratings'] = steam_df['positive_ratings'] + steam_df['negative_ratings']
+    # sb.histplot(data=steam_df[steam_df['total_ratings'] >= 50], x='review_score', bins=40).set(
+    #     title="Review Score Distribution",
+    #     ylabel="Count",
+    #     xlabel="Review Score",
+    # )
+    # plt.show()
+
+    reviews_df = pd.read_csv('data/reviews.csv')
+
+    bins = [0, 60, 120, 240, 480, 1200, 2400, 4800, 12000, 24000, 48000, 120000, 240000, 480000, 1200000]
+    names = []
+
+    for i in range(len(bins) - 1):
+        low = shorten_num(bins[i])
+        high = shorten_num(bins[i + 1])
+        names.append(low + '-' + high)
+
+    reviews_df['playtime_binned'] = pd.cut(reviews_df['playtime_at_review'], bins)
+
+    print(reviews_df.dtypes)
+
+    df = reviews_df.groupby(['playtime_binned', 'recommended'])['appid'].count().rename('rec_count').reset_index()
+    df['rec_count'] = df['rec_count'].div(df.groupby('playtime_binned')['rec_count'].transform(lambda x: x.sum())).mul(100)
+
+    # Percentage of recommendations per time played
+    g = sb.barplot(data=df, x='playtime_binned', hue='recommended', y='rec_count', 
+        hue_order=[True], palette=['forestgreen'])
+    g.set(
+        ylim=(0, 100),
+        xlabel='Playtime at Review (in minutes)',
+        ylabel='Recommended (%)',
+        title='Percentage of Positive Reviews by Playtime'
+    )
+    g.get_legend().remove()
+    g.bar_label(g.containers[0], fmt='%.1f')
+    g.set_xticklabels(names)
     plt.show()
 
     # Price distribution
@@ -84,6 +128,8 @@ def main():
     g.set(title="Owners Distribution", ylabel="Count", xlabel="Number of Owners")
     g.bar_label(g.containers[0])
     plt.show()
+    
+    proton_df = pd.read_csv('data/proton_db.csv')
 
     # ProtonDB rating histogram
     sb.countplot(
@@ -117,6 +163,8 @@ def main():
     g.set(title='ProtonDB tier distribution per release year')
     g.get_legend().set_title('ProtonDB Tier')
     plt.show()
+
+    hltb_df = pd.read_csv('data/hltb.csv')
 
     merged = pd.merge(steam_df, hltb_df, on='appid')
 
