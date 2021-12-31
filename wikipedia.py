52067190bbee465e96c25efbda59d63c9a53a5ac
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-12-31 19:18:39 +0000

    Finish Wikipedia script (raw data only)

--- wikipedia.py
+++ wikipedia.py
@@ -18,7 +18,7 @@ def write_data(df, messages=True):
         if messages:
             print(Fore.CYAN + '- Writing updated data to new CSV file...')
         
-        df.to_csv('data/wikipedia.csv', index=False)
+        df.to_json('data/wikipedia_raw.json', orient='records')
 
 def main():
     colorama.init()
@@ -31,7 +31,8 @@ def main():
     existing_appids = set()
 
     try:
-        existing_data = pd.read_csv('data/wikipedia.csv')
+        f = open('data/wikipedia_raw.json', 'r')
+        existing_data = pd.read_json(f, orient='records')
 
         print(Fore.YELLOW + '- Found existing file, skipping API calls for existing games...')
         wp = wp.append(existing_data)
@@ -43,51 +44,86 @@ def main():
         'from Wikipedia API...\n' + Fore.RESET)
 
     url = 'https://en.wikipedia.org/w/api.php'
-    backoff = 0.5
+    df['release_year'] = df['release_date'].apply(lambda x: x[:4])
 
     for index, row in tqdm(list(df.iterrows())):
         appid = row['appid']
         name = row['name']
+        year = row['release_year']
 
         if appid in existing_appids:
             continue
 
-        response = requests.get(url, params={
+        # Attempt to query these two titles, if both fail query title with just the game name
+        opt = [f'{name} ({year} video game)', f'{name} (video game)']
+        opt_str = '|'.join(opt)
+
+        extract = None
+        params = {
             'action': 'query',
             'prop': 'extracts',
             'exsectionformat': 'plain',
             'explaintext': 'true',
-            'titles': name,
+            'titles': opt_str,
             'format': 'json',
             'formatversion': '2',
-        })
+        }
 
-        if response.status_code == 200:
-            jsr = json.loads(response.text)
+        def perform_request():
+            nonlocal url, params, extract
+            backoff = 0.5
 
-            if 'errors' in jsr:
-                rate_limited = False
-                for error in jsr['errors']:
-                    if error['code'] == 'ratelimited':
-                        rate_limited = True
-                        break
+            while True:
+                response = requests.get(url, params=params)
                 
-                if rate_limited:
-                    print(Fore.RED + f'Rate limited! Sleeping for {backoff}s'
-                        + Fore.RESET)
-                    sleep(backoff)
-                    backoff *= 2
-            else:
-                # TODO: Parse JSON response
-                json.dump(jsr, open('data/wiki_test.json', 'w'), indent=2)
-        else:
-            print(Fore.RED + response.text + Fore.RESET)
+                if response.status_code == 200:
+                    jsr = json.loads(response.text)
 
-        exit(0) # FIXME: Remove
+                    if 'errors' in jsr:
+                        rate_limited = False
+                        for error in jsr['errors']:
+                            if error['code'] == 'ratelimited':
+                                rate_limited = True
+                                break
+                        
+                        if rate_limited:
+                            print(Fore.RED + f'Rate limited! Sleeping for {backoff}s'
+                                + Fore.RESET)
+                            sleep(backoff)
+                            backoff *= 2
+                        else:
+                            # Another kind of error in the response, abort
+                            return
+                    else:
+                        # No errors in response
+                        break
+                else:
+                    print(Fore.RED + f'Error: {response.status_code}' + Fore.RESET)
+                    return
+            
+            pages = jsr['query']['pages']
+            for page in pages:
+                if 'extract' in page:
+                    extract = page['extract']
+                    break
         
+        perform_request()
+        if extract == None:
+            params['titles'] = name
+            perform_request()
+
+        wp = wp.append({
+            'appid': appid,
+            'wp_article': extract,
+        }, ignore_index=True)
+
+        # Sleep for 250ms between each request to avoid rate limiting
+        sleep(0.25)
+
         if index % 50 == 0:
             write_data(wp, messages=False)
 
+    print()
     write_data(wp)
     print(Fore.GREEN + '\nDone.\n' + Style.RESET_ALL)
 
