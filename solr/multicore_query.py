55cc3d374adf881d86f6719a2f3347287de09092
Clara Martins <clara.alves.martins@hotmail.com>
2022-01-13 11:37:52 +0000

    Queries from files

--- solr/multicore_query.py
+++ solr/multicore_query.py
@@ -1,5 +1,6 @@
 
 import requests, json
+from os.path import exists as file_exists
 from argparse import ArgumentParser
 from rich import print
 from rich.pretty import pprint
@@ -56,17 +57,6 @@ def print_review(review):
     pprint(review['review'], max_string=500)
     print('\n' + '\u2500' * 80 + '\n')
 
-def select_int(min_int, max_int):
-    print(f'Select an integer in the interval: [{min_int}-{max_int}]')
-    while True:
-        try:
-            index = int(input(' -> '))
-            if min_int <= index <= max_int:
-                return index
-            print(Fore.RED + 'Out of range' + Fore.RESET)
-        except ValueError:
-            print(Fore.RED + 'Not an integer' + Fore.RESET)
-
 def query(q, core):
     '''
     Returns (n_found, documents), where documents are the solr_documents
@@ -86,35 +76,49 @@ def query(q, core):
     n_found = response['response']['numFound']
     return (n_found, documents)
 
-def multicore_query(q_str):
-    '''
-    Returns (n_found, document_tuples) where document_tuples is a list of tuples: (solr_document, origin_core)
-    '''
-    q_games = {
-        'query': {
-            'edismax': {
-                'query': q_str,
-                'q.op': 'AND',
-                'qf': 'name^10 developer publisher categories^1.5 genres^3 steamspy_tags^2 \
-                        about_the_game^1.5 short_description detailed_description^0.8',
-                'boost': 'mul(weighted_score, sqrt(log(total_ratings)))',
-                'fl': '*, score',
-            }
-        }
+def fill(request, core):
+    default_qf = {
+        GAMES: 'name^10 developer publisher categories^1.5 genres^3 steamspy_tags^2 \
+        about_the_game^1.5 short_description detailed_description^0.8',
+        REVIEWS: 'review^4 steamspy_tags^2 developer name'
     }
-    q_reviews = {
-        'query': {
-            'edismax': {
-                'query': q_str,
-                'q.op': 'AND',
-                'qf': 'review^4 steamspy_tags^2 developer name',
-                'boost': 'vote_score',
-                'fl': '*, score',
-            }
-        }
+
+    default_boost = {
+        GAMES: 'mul(weighted_score, sqrt(log(total_ratings)))',
+        REVIEWS: 'vote_score'
     }
-    (n_found_games  ,  games_results) = query(q_games  , 'games'  )
-    (n_found_reviews, review_results) = query(q_reviews, 'reviews')
+
+    if not request['query']['edismax']['qf']:
+        request['query']['edismax']['qf'] = default_qf[core]
+
+    if not request['query']['edismax']['boost']:
+        request['query']['edismax']['boost'] = default_boost[core]
+
+def single_core_query(obj):
+    global default_qf, default_boost
+
+    core = obj['core']
+    q    = obj['request']
+
+    fill(q, core)
+    
+    (n_found, results) = query(q, core)
+    return (n_found, [(result, core) for result in results])
+
+def multi_core_query(obj):
+    global default_qf, default_boost
+
+    q = obj['q']
+    q_games   = obj['request_games'  ]
+    q_reviews = obj['request_reviews']
+    q_games  ['query']['edismax']['query'] = q
+    q_reviews['query']['edismax']['query'] = q
+    
+    fill(q_games  , GAMES  )
+    fill(q_reviews, REVIEWS)
+
+    (n_found_games  ,  games_results) = query(q_games  , GAMES  )
+    (n_found_reviews, review_results) = query(q_reviews, REVIEWS)
     
     n_found = n_found_games + n_found_reviews
 
@@ -128,192 +132,29 @@ def multicore_query(q_str):
 
     return (n_found, result_tuples)
 
-def single_query():
-    qf_games = 'name^10 developer publisher categories^1.5 genres^3 steamspy_tags^2 \
-                about_the_game^1.5 short_description detailed_description^0.8'
-    boost_games = 'mul(weighted_score, sqrt(log(total_ratings)))'
-
-    qf_reviews = 'review^4 steamspy_tags^2 developer name'
-    boost_reviews = 'vote_score'
-
-    query_list = [
-        # (Query, Core)
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'story-rich rpg "turn based combat" price:[* TO 20}',
-                    'q.op': 'AND',
-                    'qf': 'name^10 genres^3 steamspy_tags^2 detailed_description',
-                    'boost': 'weighted_score',
-                }
-            }
-        }, GAMES ), # Listing 1
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'fighting weighted_score:{0.6 to *]',
-                    'q.op': 'AND',
-                    'boost': 'weighted_score', # TODO: check if the query had boost or not
-                }
-            }
-        }, GAMES ), # Table 1
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'fighting weighted_score:{0.6 to *]',
-                    'q.op': 'AND',
-                    'qf': 'steamspy_tags^2 short_description^1.5 detailed_description',
-                    'boost': 'weighted_score', # TODO: check if the query had boost or not
-                }
-            }
-        }, GAMES ), # Table 2
-        ( {
-           'query': {
-                'edismax': {
-                    'query': 'steamspy_tags:Difficult',
-                    'boost': 'review_score',
-                }
-            } 
-        }, GAMES ), # Table 3
-        ( {
-           'query': {
-                'edismax': {
-                    'query': 'steamspy_tags:Difficult',
-                    'boost': 'mul(review_score, log(total_ratings))',
-                }
-            } 
-        }, GAMES ), # Table 4
-        ( {
-           'query': {
-                'edismax': {
-                    'query': 'steamspy_tags:Difficult',
-                    'boost': 'weighted_score',
-                }
-            } 
-        }, GAMES ), # Table 5
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'gta',
-                    'q.op': 'AND',
-                    'qf': 'name^10 genres^3 steamspy_tags^2 detailed_description',
-                    'boost': 'weighted_score',
-                }
-            }
-        }, GAMES ), # Listing 6 and 7 # TODO: check
-
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'story sci-fi rpg combat price:[* TO 25]',
-                    'q.op': 'AND',
-                    'qf': qf_games,
-                    'boost': boost_games,
-                }
-            }
-        }, GAMES ), # Evaluation 1
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'historical command army',
-                    'q.op': 'AND',
-                    'qf': qf_games,
-                    'boost': boost_games,
-                }
-            }
-        }, GAMES ), # Evalutation 2
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'immersion "good graphics" votes_up:[3 TO *]',
-                    'q.op': 'AND',
-                    'qf': qf_reviews,
-                    'boost': boost_reviews,
-                }
-            }
-        }, REVIEWS ), # Evaluation 3
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'survival zombies votes_funny:[5 TO *]',
-                    'q.op': 'AND',
-                    'qf': qf_reviews,
-                    'boost': boost_reviews,
-                }
-            }
-        }, REVIEWS ), # Evaluation 4
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'strategy army steamspy_tags:Medieval',
-                    'q.op': 'AND',
-                    'qf': qf_games,
-                    'boost': boost_games,
-                    'rows': 20,
-                }
-            }
-        }, GAMES ), # Evaluation 5
-        ( {
-            'query': {
-                'edismax': {
-                    'query': 'fantasy genres:RPG weighted_score:[0.92 TO *]',
-                    'q.op': 'AND',
-                    'qf': qf_games,
-                    'boost': boost_games,
-                }
-            }
-        }, GAMES ), # Evaluation 6
-    ]
-
-    q = select_int(0, len(query_list) - 1)
-    (solr_query, solr_core) = query_list[q]
-    (n_found, documents) = query(solr_query, solr_core)
-    
-    print(f'Found {n_found} documents.\n')
-    for doc in documents:
-        print_result(doc, solr_core)
-
-def double_query(q=None):
-    if not q:
-        query_list = [
-            {
-                'query': {
-                    'edismax': {
-                        'query': 'msgs',
-                        'q.op': 'AND',
-                        'qf': '', # TODO
-                        'boost': '', # TODO
-                        'fl': '*, score',
-                    }
-                }
-            }, # Multicore Query 1: msgs
-        ]
-
-        q = select_int(0, len(query_list) - 1)
-        q = query_list[q]
-    
-    (n_found, documents) = multicore_query(q)
-    
-    print(f'Found {n_found} documents.\n')
-    for doc, doc_type in documents:
-        print_result(doc, doc_type)
-
 def main():
     parser = ArgumentParser()
-    parser.add_argument('query', nargs='?', help='query')
+    parser.add_argument('query_file', help='name of the query\'s file, without the extension (on the solr/queries folder)')
     args = parser.parse_args()
-    if args.query:
-        double_query(args.query)
-        return 0
 
-    print('[1] Single Core Query')
-    print('[2] Multi Core Query')
-    menu = select_int(1, 2)
-    if menu == 1:
-        single_query()
-    elif menu == 2:
-        double_query()
-    return 0
+    file_name = f'solr/queries/{args.query_file}.json'
+    if file_exists(file_name):
+        file = open(file_name)
+        query_json = json.load(file)
+        file.close()
+
+        # (n_found, results) = (0, [])
+        if query_json['core'] == None:
+            (n_found, results) = multi_core_query(query_json)
+        else:
+            (n_found, results) = single_core_query(query_json)
+
+        print(f'[b][green]Found {n_found} documents.[/green][/b]\n')
+        for result, result_type in results:
+            print_result(result, result_type)
+
+    else:
+        print('[b][red]Invalid File[/red][/b]')
 
 if __name__ == '__main__':
     main()
